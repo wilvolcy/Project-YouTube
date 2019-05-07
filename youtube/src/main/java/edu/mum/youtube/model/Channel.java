05a9a8aef0f2248639a094560d6a893c067e51d0
wilvolcy <50376806+wilvolcy@users.noreply.github.com>
2019-05-07 17:43:18 -0500

    Create Channel.java

---
+++ youtube/src/main/java/edu/mum/youtube/model/Channel.java
@@ -0,0 +1,15 @@
+package edu.mum.youtube.model;
+
+import java.util.List;
+
+public class Channel {
+
+	private long id;
+	private String  titile;
+	private String description;
+	private UserAccount userAccount ;
+	private List<Subscription> listSubscription;
+	private List<Video> listVideo;
+	
+	
+}
