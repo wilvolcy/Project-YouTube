fb42933f39331b42cc83309ac4c9cc2dbce87148
wilvolcy <50376806+wilvolcy@users.noreply.github.com>
2019-05-07 16:02:05 -0500

    Create YoutubeApplicationTests.java

---
+++ youtube/src/test/java/edu/mum/youtube/youtube/YoutubeApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.mum.youtube.youtube;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YoutubeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
