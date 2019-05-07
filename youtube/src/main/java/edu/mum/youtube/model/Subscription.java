f6c86861175e5a59ce8e3ad0d1f123ff60853a0e
wilvolcy <50376806+wilvolcy@users.noreply.github.com>
2019-05-07 17:43:41 -0500

    Create Subscription.java

---
+++ youtube/src/main/java/edu/mum/youtube/model/Subscription.java
@@ -0,0 +1,13 @@
+package edu.mum.youtube.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Subscription {
+private long id;
+private LocalDate subscriptionDate;
+private Channel channel;
+private UserAccount userAccount;
+
+
+}
