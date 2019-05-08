c32d71b37e6747976749b242ae412d82f07d7d14
wilvolcy <50376806+wilvolcy@users.noreply.github.com>
2019-05-07 23:37:33 -0500

    Web ok V1.2

--- youtube/src/main/java/edu/mum/youtube/RestController/ControllerClass.java
+++ youtube/src/main/java/edu/mum/youtube/RestController/ControllerClass.java
@@ -1,9 +1,9 @@
 package edu.mum.youtube.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
- 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
  
 
@@ -12,8 +12,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller 
 public class ControllerClass {
 	
-	@RequestMapping("/welcome")
-	public String Hello() {
+	@RequestMapping("/home")
+	public String Welcome(HttpServletRequest request) {
+		request.setAttribute("mode", "MODE_HOME");
 		return "home";
 	}
+	
+	@RequestMapping("/register")
+	public String registration(HttpServletRequest request) {
+		request.setAttribute("mode", "MODE_REGISTER");
+		return "home";
+	}
+	
 }
--- youtube/src/main/webapp/WEB-INF/view/home.jsp
+++ youtube/src/main/webapp/WEB-INF/view/home.jsp
@@ -1,5 +1,5 @@
 <!DOCTYPE html >
- 
+ <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
 <head>
 <meta charset="utf-8">
@@ -31,7 +31,7 @@
 	</div>
 
 	<c:choose>
-		<c:when test="${mode=='MODE_HOME' }">
+		<c:when test="${mode=='MODE_HOME'}">
 			<div class="container" id="homediv">
 				<div class="jumbotron text-center">
 					<h1>Welcome to YouTube </h1>
